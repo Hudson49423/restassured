cbd135ff24d2e05cb5b1153b28894c39c4f036a1
Hudson Clark <Hudson49423@gmail.com>
2015-06-15 22:03:34 -0400

    set up logic for coloring the grid items

--- app/src/main/java/com/octopusbeach/restassured/GridAdapter.java
+++ app/src/main/java/com/octopusbeach/restassured/GridAdapter.java
@@ -1,6 +1,9 @@
 package com.octopusbeach.restassured;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.LayerDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,15 +12,34 @@ import android.widget.TextView;
 
 import com.octopusbeach.restassured.model.Item;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by hudson on 6/15/15.
  */
 public class GridAdapter extends ArrayAdapter<Item> {
+    private int[] colors;
+    private Random rand;
 
     public GridAdapter(Context context, int resource, List<Item> objects) {
         super(context, resource, objects);
+        Resources res = context.getResources();
+        colors = new int[]{
+                res.getColor(R.color.cyan),
+                res.getColor(R.color.yellow),
+                res.getColor(R.color.teal),
+                res.getColor(R.color.blue),
+                res.getColor(R.color.grey),
+                res.getColor(R.color.orange),
+                res.getColor(R.color.indigo),
+                res.getColor(R.color.pink),
+                res.getColor(R.color.purple),
+                res.getColor(R.color.red)
+        };
+        rand = new Random();
     }
 
     @Override
@@ -27,8 +49,26 @@ public class GridAdapter extends ArrayAdapter<Item> {
             LayoutInflater inflater = LayoutInflater.from(getContext());
             v = inflater.inflate(R.layout.grid_item, null);
         }
+
         Item item = getItem(position);
-        ((TextView)v.findViewById(R.id.item_title)).setText(item.getName());
+        TextView title = (TextView) v.findViewById(R.id.item_title);
+        TextView completed = (TextView) v.findViewById(R.id.time);
+
+        title.setText(item.getName());
+        Calendar date = item.getDate();
+        Calendar rightNow = Calendar.getInstance();
+        if (date.get(Calendar.DAY_OF_YEAR) == rightNow.get(Calendar.DAY_OF_YEAR)
+                && date.get(Calendar.YEAR) == rightNow.get(Calendar.YEAR)) // Today
+            completed.setText(" Today at " + new SimpleDateFormat("h:mm a").format(date.getTime()));
+        else if (date.get(Calendar.DAY_OF_YEAR) == (rightNow.get(Calendar.DAY_OF_YEAR) - 1)
+                && date.get(Calendar.YEAR) == rightNow.get(Calendar.YEAR))  // Yesterday
+            completed.setText(" Yesterday at " + new SimpleDateFormat("h:mm a").format(date.getTime()));
+        else
+            completed.setText(" " + new SimpleDateFormat("MMMM dd, h:mm a").format(date.getTime()));
+
+        LayerDrawable bg = (LayerDrawable) v.findViewById(R.id.parent).getBackground();
+        GradientDrawable bgShape = (GradientDrawable)bg.findDrawableByLayerId(R.id.background);
+        bgShape.setColor(colors[rand.nextInt(colors.length)]);
         return v;
     }
 }
